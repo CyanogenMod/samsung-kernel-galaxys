7496b076f80615dde29b01e0c1f7fd02df527ec8
Nick Pelly <npelly@google.com>
2009-12-08 19:42:21 -0800

    Bluetooth: Introduce L2CAP_LM_FLUSHABLE to allow flushing of ACL packets.
    With Bluetooth 2.1 ACL packets can be flushable or non-flushable. This changes
    makes the default ACL packet non-flushable, and allows selection of flushable
    packets on a per-L2CAP socket basis with L2CAP_LM_FLUSHABLE.
    
    Note the HCI Write Automatic Flush Timeout command also needs to be issued
    to set the flush timeout to non-zero.
    
    Need to featurize this change to Bluetooth 2.1 chipsets only before pushing
    upstream.
    
    Signed-off-by: Nick Pelly <npelly@google.com>

--- net/bluetooth/l2cap.c
+++ net/bluetooth/l2cap.c
@@ -325,7 +325,7 @@ static inline int l2cap_send_cmd(struct l2cap_conn *conn, u8 ident, u8 code, u16
 	if (!skb)
 		return -ENOMEM;
 
-	return hci_send_acl(conn->hcon, skb, 0);
+	return hci_send_acl(conn->hcon, skb, ACL_START);
 }
 
 static void l2cap_do_start(struct sock *sk)
@@ -714,12 +714,14 @@ static void l2cap_sock_init(struct sock *sk, struct sock *parent)
 		pi->sec_level = l2cap_pi(parent)->sec_level;
 		pi->role_switch = l2cap_pi(parent)->role_switch;
 		pi->force_reliable = l2cap_pi(parent)->force_reliable;
+		pi->flushable = l2cap_pi(parent)->flushable;
 	} else {
 		pi->imtu = L2CAP_DEFAULT_MTU;
 		pi->omtu = 0;
 		pi->sec_level = BT_SECURITY_LOW;
 		pi->role_switch = 0;
 		pi->force_reliable = 0;
+		pi->flushable = 0;
 	}
 
 	/* Default config options */
@@ -1116,6 +1118,7 @@ static inline int l2cap_do_send(struct sock *sk, struct msghdr *msg, int len)
 	struct sk_buff *skb, **frag;
 	int err, hlen, count, sent=0;
 	struct l2cap_hdr *lh;
+	u16 flags;
 
 	BT_DBG("sk %p len %d", sk, len);
 
@@ -1168,7 +1171,12 @@ static inline int l2cap_do_send(struct sock *sk, struct msghdr *msg, int len)
 		frag = &(*frag)->next;
 	}
 
-	if ((err = hci_send_acl(conn->hcon, skb, 0)) < 0)
+	if (l2cap_pi(sk)->flushable)
+		flags = ACL_START_FLUSHABLE;
+	else
+		flags = ACL_START;
+
+	if ((err = hci_send_acl(conn->hcon, skb, flags)) < 0)
 		goto fail;
 
 	return sent;
@@ -1277,6 +1285,7 @@ static int l2cap_sock_setsockopt_old(struct socket *sock, int optname, char __us
 
 		l2cap_pi(sk)->role_switch    = (opt & L2CAP_LM_MASTER);
 		l2cap_pi(sk)->force_reliable = (opt & L2CAP_LM_RELIABLE);
+		l2cap_pi(sk)->flushable = (opt & L2CAP_LM_FLUSHABLE);
 		break;
 
 	default:
@@ -1402,6 +1411,9 @@ static int l2cap_sock_getsockopt_old(struct socket *sock, int optname, char __us
 
 		if (l2cap_pi(sk)->force_reliable)
 			opt |= L2CAP_LM_RELIABLE;
+
+		if (l2cap_pi(sk)->flushable)
+			opt |= L2CAP_LM_FLUSHABLE;
 
 		if (put_user(opt, (u32 __user *) optval))
 			err = -EFAULT;
@@ -2613,7 +2625,7 @@ static int l2cap_recv_acldata(struct hci_conn *hcon, struct sk_buff *skb, u16 fl
 
 	BT_DBG("conn %p len %d flags 0x%x", conn, skb->len, flags);
 
-	if (flags & ACL_START) {
+	if (!(flags & ACL_CONT)) {
 		struct l2cap_hdr *hdr;
 		int len;
 
